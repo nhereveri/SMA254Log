a8e9dde77a0e91223385b2c27377d8e1087c15c6
Nelson Hereveri <nelson@hereveri.cl>
2022-02-08 21:49:55 -0400

    Revert "Modify to JSONAPI output"
    This reverts commit 7babb600bf1e910734782376e783f366c49465ca.

--- src/SMA254LogController.php
+++ src/SMA254LogController.php
@@ -5,32 +5,17 @@ namespace Airviro\SMA254Log;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Airviro\SMA254Log\SMA254Log;
-use Illuminate\Support\Facades\URL;
 
 class SMA254LogController extends Controller
 {
     public function index()
     {
-        $procesos = SMA254Log::orderBy('unixtime', 'desc')->cursorPaginate(env('SMA254LOG_PAGINATION', 100));
-        return response()->json(array(
-            'links' => [
-                'prev_page_url' => $procesos->previousPageUrl(),
-                'next_page_url' => $procesos->nextPageUrl(),
-                'self' => URL::full()
-            ],
-            'data' => $procesos->all()
-        ), 200)->header('Content-Type', 'application/vnd.api+json');
+        return SMA254Log::orderBy('unixtime', 'desc')->cursorPaginate(env('SMA254LOG_PAGINATION', 100));
     }
 
     public function show($id)
     {
-        $proceso = SMA254Log::findOrFail($id);
-        return response()->json(array(
-            'links' => [
-                'self' => URL::full()
-            ],
-            'data' => $proceso->all()
-        ), 200)->header('Content-Type', 'application/vnd.api+json');
+        return SMA254Log::findOrFail($id);
     }
 
     public function proceso($uf, $proceso)
@@ -46,19 +31,6 @@ class SMA254LogController extends Controller
     public function procesoFromTo($uf, $proceso, $from, $to)
     {
         return SMA254Log::where('uf', '=', $uf)->where('proceso', '=', $proceso)->where('unixtime', '>=', $from)->where('unixtime', '<=', $to)->orderBy('unixtime', 'asc')->cursorPaginate(env('SMA254LOG_PAGINATION', 100));
-    }
-
-    public function procesoLast($uf, $proceso)
-    {
-        $procesos = $this->proceso($uf, $proceso);
-        return response()->json(array(
-            'links' => [
-                'prev_page_url' => $procesos->previousPageUrl(),
-                'next_page_url' => $procesos->nextPageUrl(),
-                'self' => URL::full()
-            ],
-            'data' => $procesos->all()
-        ), 200)->header('Content-Type', 'application/vnd.api+json');
     }
 
     public function dispositivo($uf, $proceso, $dispositivo)
@@ -80,14 +52,7 @@ class SMA254LogController extends Controller
                 }
             }
         }
-        return response()->json(array(
-            'links' => [
-                'prev_page_url' => $procesos->previousPageUrl(),
-                'next_page_url' => $procesos->nextPageUrl(),
-                'self' => URL::full()
-            ],
-            'data' => $output
-        ), 200)->header('Content-Type', 'application/vnd.api+json');
+        return response()->json($output, 200);
     }
 
     public function parametro($uf, $proceso, $dispositivo, $parametro)
@@ -115,84 +80,19 @@ class SMA254LogController extends Controller
                 }
             }
         }
-        return response()->json(array(
-            'links' => [
-                'prev_page_url' => $procesos->previousPageUrl(),
-                'next_page_url' => $procesos->nextPageUrl(),
-                'self' => URL::full()
-            ],
-            'data' => $output
-        ), 200)->header('Content-Type', 'application/vnd.api+json');
+        return response()->json($output, 200);
     }
 
     public function from($uf, $proceso, $dispositivo, $parametro, $from)
     {
         $output = array();
-        $procesos = $this->procesoFrom($uf, $proceso, $from);
-        foreach($procesos as $proceso) {
-            foreach($proceso->data as $data) {
-                if($data['dispositivoId'] == $dispositivo) {
-                    foreach($data['parametros'] as $par) {
-                        if($par['nombre'] == $parametro) {
-                            array_push($output, [
-                                'unixtime' => $proceso->unixtime,
-                                'uf' => $proceso->uf,
-                                'proceso' => $proceso->proceso,
-                                'id' => $proceso->id,
-                                'dispositivo' => $data['dispositivoId'],
-                                'parametro' => $par['nombre'],
-                                'unidad' => $par['unidad'],
-                                'valor' => $par['valor'],
-                                'enviado' => $proceso->enviado
-                            ]);
-                        }
-                    }
-                }
-            }
-        }
-        return response()->json(array(
-            'links' => [
-                'prev_page_url' => $procesos->previousPageUrl(),
-                'next_page_url' => $procesos->nextPageUrl(),
-                'self' => URL::full()
-            ],
-            'data' => $output
-        ), 200)->header('Content-Type', 'application/vnd.api+json');
+        return $this->procesoFrom($uf, $proceso, $from);
     }
 
     public function fromTo($uf, $proceso, $dispositivo, $parametro, $from, $to)
     {
         $output = array();
-        $procesos = $this->procesoFromTo($uf, $proceso, $from, $to);
-        foreach($procesos as $proceso) {
-            foreach($proceso->data as $data) {
-                if($data['dispositivoId'] == $dispositivo) {
-                    foreach($data['parametros'] as $par) {
-                        if($par['nombre'] == $parametro) {
-                            array_push($output, [
-                                'unixtime' => $proceso->unixtime,
-                                'uf' => $proceso->uf,
-                                'proceso' => $proceso->proceso,
-                                'id' => $proceso->id,
-                                'dispositivo' => $data['dispositivoId'],
-                                'parametro' => $par['nombre'],
-                                'unidad' => $par['unidad'],
-                                'valor' => $par['valor'],
-                                'enviado' => $proceso->enviado
-                            ]);
-                        }
-                    }
-                }
-            }
-        }
-        return response()->json(array(
-            'links' => [
-                'prev_page_url' => $procesos->previousPageUrl(),
-                'next_page_url' => $procesos->nextPageUrl(),
-                'self' => URL::full()
-            ],
-            'data' => $output
-        ), 200)->header('Content-Type', 'application/vnd.api+json');
+        return $this->procesoFromTo($uf, $proceso, $from, $to);
     }
 
     public function highcharts($uf, $proceso, $dispositivo, $parametro, $from, $to)
@@ -218,12 +118,9 @@ class SMA254LogController extends Controller
             }
         }
         return response()->json(array(
-            'links' => [
-                'prev_page_url' => $procesos->previousPageUrl(),
-                'next_page_url' => $procesos->nextPageUrl(),
-                'self' => URL::full()
-            ],
+            'prev_page_url' => $procesos->previousPageUrl(),
+            'next_page_url' => $procesos->nextPageUrl(),
             'data' => $output
-        ), 200)->header('Content-Type', 'application/vnd.api+json');
+        ), 200);
     }
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -3,7 +3,7 @@
 Route::prefix('api')->group(function () {
 	Route::get('sma254log', 'Airviro\SMA254Log\SMA254LogController@index');
 	Route::get('sma254log/{id}', 'Airviro\SMA254Log\SMA254LogController@show');
-	Route::get('sma254log/{uf}/{proceso}', 'Airviro\SMA254Log\SMA254LogController@procesoLast');
+	Route::get('sma254log/{uf}/{proceso}', 'Airviro\SMA254Log\SMA254LogController@proceso');
 	Route::get('sma254log/{uf}/{proceso}/{dispositivo}', 'Airviro\SMA254Log\SMA254LogController@dispositivo');
 	Route::get('sma254log/{uf}/{proceso}/{dispositivo}/{parametro}', 'Airviro\SMA254Log\SMA254LogController@parametro');
 	Route::get('sma254log/{uf}/{proceso}/{dispositivo}/{parametro}/{from}', 'Airviro\SMA254Log\SMA254LogController@from');
